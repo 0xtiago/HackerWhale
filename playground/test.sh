9d4bb50c5e70f2c680d23a0beeeb211fa1daf519
Tiago Tavares <tiagotvrs@gmail.com>
2024-07-30 20:13:45 -0300

    Expansible

--- playground/test.sh
+++ playground/test.sh
@@ -8,8 +8,8 @@ echo "[+] Amass"
 go install -v github.com/owasp-amass/amass/v4/...@master
 
 
-echo "[+] Amass"
-sudo apt install apache2 -y
+echo "[+] Apache"
+apt install apache2 -y
 apachectl -v
-sudo systemctl start apache2
+systemctl start apache2
 curl -I http://localhost
